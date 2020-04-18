e7c007bb82c42714df21332439abb5e587f2c76a
ForaceGump <themattavant@gmail.com>
2020-04-17 18:40:09 -0700

    Made the program automatically show the user the eventdata in each timeblock from local storage

--- script.js
+++ script.js
@@ -35,14 +35,24 @@ $(document).ready(function(){
 
 
 $(".save").on("click", function(){
-    var hourEvent = $(this).siblings(".event").val();
-    var saveHour = $(this).parent().attr("id");
+    var hourEvent = $(this).parent().parent().children(".rowData").children(".event").val();
+    var saveHour = $(this).parent().parent().attr("id");
+
+    console.log($(this).parent().parent().children(".rowData").children(".event").val());
+    console.log(saveHour);
 
     localStorage.setItem(saveHour, hourEvent);
 });
 
-//$("#9-row .event").val()
-
+$("#9-row .event").val(localStorage.getItem("9-row"));
+$("#10-row .event").val(localStorage.getItem("10-row"));
+$("#11-row .event").val(localStorage.getItem("11-row"));
+$("#12-row .event").val(localStorage.getItem("12-row"));
+$("#13-row .event").val(localStorage.getItem("13-row"));
+$("#14-row .event").val(localStorage.getItem("14-row"));
+$("#15-row .event").val(localStorage.getItem("15-row"));
+$("#16-row .event").val(localStorage.getItem("16-row"));
+$("#17-row .event").val(localStorage.getItem("17-row"));
 
 
 
