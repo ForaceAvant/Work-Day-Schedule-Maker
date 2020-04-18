ae157bbe262b6bb125fb2786ec753785c6ebec21
ForaceGump <themattavant@gmail.com>
2020-04-17 18:00:26 -0700

    Fixed updater to set row color to indicate current time.

--- script.js
+++ script.js
@@ -4,58 +4,31 @@ var datetime = null,
 var update = function () {
     date = moment(new Date())
     datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-    var nowCurrent = date.format("H");
-    console.log(moment().hour());
     
-    if(moment().hour() == 9){
-        $("#row-9").attr("background-color", "green")
-    }
+    var currentTime = moment().hour();
 
-    if(moment().hour() == 10){
-        $("#row-9").attr("background-color", "grey");
-        $("#row-10").attr("background-color", "green");
-        
-    }
-    if(moment().hour() == 11){
-        $("#row-10").attr("background-color", "grey");
-        $("#row-11").attr("background-color", "green");
-    }
-    if(moment().hour() == 12){
-        $("#row-11").attr("background-color", "grey");
-        $("#row-12").attr("background-color", "green");
-    }
-    if(moment().hour() == 13){
-        $("#row-12").attr("background-color", "grey");
-        $("#row-1").attr("background-color", "green");
-    }
 
-    if(moment().hour() == 14){
-        $("#row-1").attr("background-color", "grey");
-        $("#row-2").attr("background-color", "green");
-    }
+    $(".time-row").each(function(){
+        var rowsTime = parseInt($(this).attr("id").split("-")[0]);
 
-    if(moment().hour() >= 15){
-        $("#row-2").attr("style", "background-color : grey");
-        $("#row-3").attr("style", "background-color : green");
-    }
-    if(moment().hour() == 16){
-        $("#row-3").attr("background-color", "grey");
-        $("#row-4").attr("background-color", "green");
-    }
-    if(moment().hour() == 17){
-        $("#row-4").attr("background-color", "grey");
-        $("#row-5").attr("background-color", "green");
-    }
-    if(moment().hour() == 18){
-        $("#row-5").attr("background-color", "grey");
-        
-    }
+        if(rowsTime < currentTime){
+            $(this).attr("style", "background-color : grey");
+        }
+
+        else if(rowsTime == currentTime){
+            $(this).attr("style", "background-color : white");
+        }
+
+        else{
+            $(this).attr("style", "background-color : green");
+        }
+    })
 };
 
 $(document).ready(function(){
     datetime = $('#time-display')
     update();
-    setInterval(update, 1000);
+    setInterval(update, 10000);
 
 
 });
@@ -67,6 +40,8 @@ $(".save").on("click", function(){
 
     localStorage.setItem(saveHour, hourEvent);
 });
+
+//$("#9-row .event").val()
 
 
 
